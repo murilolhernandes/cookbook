2c18314d5fa09c78a61feed7a71a778bcb188f31
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2025-12-19 23:14:22 -0700

    started building server.js file and adding all the node files

---
+++ server.js
@@ -0,0 +1,110 @@
+/* ******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+ *******************************************/
+/* ***********************
+ * Require Statements
+ *************************/
+const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
+const env = require("dotenv").config()
+const app = express()
+const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+// const inventoryController = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require("./database/")
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+
+/* ***********************
+ * Middleware
+*************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session)) ({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+/* ***********************
+ * Express Messages Middleware
+*************************/
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+app.use(utilities.checkJWTToken)
+
+/* ***********************
+ * Routes
+*************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
+/* ***********************
+ * Routes
+*************************/
+app.use(static)
+
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// Inventory routes
+// app.use("/inv", inventoryRoute)
+
+// Account routes
+app.use("/account", require('./routes/accountRoute'))
+
+// File Not Found Route - last route in list
+app.use(async (req, res, next) => {
+  next({
+    status: 400,
+    message: 'Sorry, we appear to have lost that page.'
+  })
+})
+
+/* ***********************
+ * Express Error Handler
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  let message
+  if (err.status == 400) {
+    message = err.message
+  } else if (process.env.NODE_ENV === 'development') {
+    message = err.message
+  } else {
+    message = 'Oh no! There was a crash. Maybe try a different route?'
+  }
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+/* ***********************
+ * Local Server Information
+*************************/
+const port = process.env.PORT
+const host = process.env.HOST
+
+/* ***********************
+ * Log statement to confirm server operation
+*************************/
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`)
+})
